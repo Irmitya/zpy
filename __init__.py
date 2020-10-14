df13bf5079720fae21b279c9735607bdc4f31bcc
Irmitya <10817631+Irmitya@users.noreply.github.com>
2020-10-14 12:52:47 -0500

    Add a an info panel, to make distributing addons easier. It's cumbersome to ask people to find and move this module to a modules folder, so I was instead copying this folder to the addons', and modifying the importing text used across all files, to have the addon need just the one folder. THAT is even more tedius, not to mention I would have to redo it everytime I update something, or manually copy only the new line changes over the distributed versions.
    This code just makes it so when you go to the preferences, blender doesn't give a
    warning about this addon not having a bl_info.
    The register/unregister is to not get an error if someone enables the addon (which is not needed).

--- __init__.py
+++ __init__.py
@@ -9,3 +9,15 @@ from . import keyframe, New, popup, Set
 # functions that are initialized()
 from .register_keymaps import register_keymaps
 from .load_modules import load_modules
+
+bl_info = {
+    "name": "Zpy Module (ignore)",
+    "author": "COnLOAR",
+    "description": "Module for various functions re-used across zpy addons",
+    "blender": (2, 90, 0),
+    "category": "System",
+    'wiki_url': "https://blenderartists.org/t/zpy-various-miscellaneous-addons/1254269",
+    "warning": "Does not need to be enabled",
+}
+
+register, unregister = bpy.utils.register_classes_factory(())
